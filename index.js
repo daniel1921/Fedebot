c479fc14bc8f024f13536c1bd83a7c6e350ea80e
Daniel Mafla Bonet <danielmaflab@hotmail.com>
2023-10-07 11:16:12 -0500

    mejoras al bot en precision de busqueda

--- index.js
+++ index.js
@@ -1,103 +1,101 @@
-const Discord = require('discord.js');
-const axios = require('axios');
-require('dotenv').config();
+const Discord = require("discord.js");
+const axios = require("axios");
+require("dotenv").config();
 
-const client = new Discord.Client({intents: 32767});
+const client = new Discord.Client({ intents: 32767 });
 
-const infiteLoop = async (j) => {
-    if(j === 100) {
-        j = 0
-        console.clear();
-    }
-    let i = j + 1
-    setTimeout(() => {
-        console.log(j)
-        infiteLoop(i); 
-    }, 5000);
-    
-    
-   
-}
 
-client.on('ready', () => {
+client.on("ready", () => {
+  console.log(`El bot esta listo, su nombre es: ${client.user.tag}`);
 
-    console.log(`El bot esta listo, su nombre es: ${client.user.tag}`)
- //   infiteLoop(0);
-// Lista De comandos
+  // Lista De comandos
 
-    client.application.commands.set([
+  client.application.commands.set([
+    {
+      name: "registro",
+      description:
+        "Este comando se utiliza para Registrarse en el discord, debe tener su mismo nick que en Albion",
+      options: [
         {
-            name: 'registro',
-            description: 'Este comando se utiliza para Registrarse en el discord, debe tener su mismo nick que en Albion',
-            options: [
-                {
-                    type: 3,
-                    name: 'user',
-                    description: 'El usuario que se desea registrar',
-                    required: true
-                }
-
-            ]
-        }
-    ])
-
-
+          type: 3,
+          name: "user",
+          description: "El usuario que se desea registrar",
+          required: true,
+        },
+      ],
+    },
+  ]);
 });
 
+//
+try {
+    client.on("interactionCreate", (int) => {
+        if (int.isCommand() && int.commandName === "registro") {
+          const nickname = int.options.getString("user");
+      
+    
+      
+          axios
+            .get(
+              `https://gameinfo.albiononline.com/api/gameinfo/search?q=${nickname}`
+            )
+      
+            .then(function (response) {
+              const playersLowerCase = [];
+      
+              response.data.players.forEach((player) => {
+                playersLowerCase.push({
+                  player: player.Name.toLowerCase(),
+                  guild: player.GuildName,
+                });
+              });
+      
+              const estaElPlayer = playersLowerCase.some(
+                (miembro) =>
+                  miembro.guild === "La Federacion Y" &&
+                  miembro.player === nickname.toLowerCase()
+              );
+      
 
-// 
-
-    client.on('interactionCreate', (int) => {
-
-        if(int.isCommand() && int.commandName === 'registro') {
-    
-            const nickname = int.options.getString('user')
-           
-           // console.log(int);
-           // console.log(int.member.roles);
-    
-           
-         
-            
-    
-            axios.get(`https://gameinfo.albiononline.com/api/gameinfo/search?q=${nickname}`)
-    
-            .then(function (response) {           
-             
-                if(response.data.players.length === 0) {
-    
-                  return  int.reply(` El usuario ${nickname}, No esta escrito correctamente, porfavor vuelve a intentarlo!`);
-    
+      
+              if (response.data.players.length === 0) {
+                return int.reply(
+                  ` El usuario ${nickname}, No esta escrito correctamente, porfavor vuelve a intentarlo!`
+                );
+              } else if (estaElPlayer) {
+                try {
+                  int.member.roles.add("955948751338471455");
+                  int.member.setNickname(nickname);
+      
+                  return int.reply(
+                    ` El usuario ${nickname}, se ha registrado en el servidor, Bienvenido! :green_heart:  `
+                  );
+                } catch (error) {
+                  return int.reply(
+                      error
+                    );
                 }
-                else if(response.data.players[0].GuildName === '')    {
-    
-                    return  int.reply(` El usuario ${response.data.players[0].Name} aun no tiene gremio, no esperes para ser parte nuestra comunidad  :beers:  `);
-                    
-                }
-                 else if(response.data.players[0].GuildName !== 'La Federacion Y' ) {
-    
-                  return  int.reply(` El usuario ${response.data.players[0].Name} actualmente esta en el gremio ${response.data.players[0].GuildName}, Si deseas unirte al gremio debes primero abandonar en el que estas. `);
-    
-                } 
-            
-                    int.member.roles.add('955948751338471455');
-                    int.member.setNickname(nickname);
-              
-               
-                            
-                return int.reply(` El usuario ${nickname}, se ha registrado en el servidor, Bienvenido! :green_heart:  `);
-              
-    
-            })
-    
-    
-           
-            
+              } else if (!estaElPlayer) {
+                return int.reply(
+                  ` Hubo un problema al momento de registrarte, probablemente no estes en el gremio, no esperes para ser parte nuestra comunidad  :beers: `
+                );
+              }
+              // else if(response.data.players[0].GuildName === '')    {
+      
+              //     return  int.reply(` El usuario ${response.data.players[0].Name} aun no tiene gremio, no esperes para ser parte nuestra comunidad  :beers:  `);
+      
+              // }
+              //  else if(response.data.players[0].GuildName !== 'La Federacion Y' ) {
+      
+              //   return  int.reply(` El usuario ${response.data.players[0].Name} actualmente esta en el gremio ${response.data.players[0].GuildName}, Si deseas unirte al gremio debes primero abandonar en el que estas. `);
+      
+              // }
+            });
         }
-    })
-
-
+      });
+} catch (error) {
+    console.log(error)
+}
 
 
 client.login(process.env.TOKEN_DISCORD);
-
